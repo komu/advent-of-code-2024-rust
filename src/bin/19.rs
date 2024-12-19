578ec4577e61be56c7521b9f6f015aaa11c447fc
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-19 09:46:15 +0200

    Optimization

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,56 +1,73 @@
-use std::collections::HashMap;
+use hashbrown::HashSet;
+use rayon::prelude::*;
 
 advent_of_code::solution!(19);
 
-fn parse(input: &str) -> (Vec<&str>, Vec<&str>) {
-    let (s1, s2) = input.split_once("\n\n").unwrap();
-
-    let patterns = s1.split(", ").collect::<Vec<_>>();
-    let designs = s2.lines().collect::<Vec<_>>();
-
-    (patterns, designs)
+struct PatternData<'a> {
+    patterns: HashSet<&'a str>,
+    max_pattern_len: usize,
 }
 
-fn ways_to_make(design: &str, patterns: &[&str]) -> u64 {
-    ways_to_make_with(design, patterns, &mut HashMap::new())
-}
+impl PatternData<'_> {
+    fn is_possible(&self, design: &str) -> bool {
+        if design.is_empty() {
+            true
+        } else {
+            let mut possible = false;
 
-fn ways_to_make_with<'a>(
-    design: &'a str,
-    patterns: &'a [&'a str],
-    cache: &mut HashMap<&'a str, u64>,
-) -> u64 {
-    if design.is_empty() {
-        1
-    } else if let Some(&value) = cache.get(design) {
-        value
-    } else {
-        let ways = patterns
-            .iter()
-            .filter(|&pattern| design.starts_with(pattern))
-            .map(|&pattern| ways_to_make_with(&design[pattern.len()..], patterns, cache))
-            .sum();
+            for i in 1..=design.len() {
+                let (prefix, suffix) = design.split_at(i);
 
-        cache.insert(design, ways);
-        ways
+                if self.patterns.contains(&prefix) && self.is_possible(suffix) {
+                    possible = true;
+                    break;
+                }
+            }
+
+            possible
+        }
+    }
+
+    fn ways_to_make(&self, design: &str) -> u64 {
+        let mut dp = vec![0; design.len() + 1];
+        dp[0] = 1;
+
+        for i in 1..=design.len() {
+            let prefix = &design[..i];
+
+            for pat_len in 1..=self.max_pattern_len.min(i) {
+                if self.patterns.contains(&prefix[i - pat_len..]) {
+                    dp[i] += dp[i - pat_len];
+                }
+            }
+        }
+
+        dp[design.len()]
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (patterns, designs) = parse(input);
+fn parse(input: &str) -> (PatternData, impl ParallelIterator<Item = &str>) {
+    let (patterns_str, designs) = input.split_once("\n\n").unwrap();
 
-    Some(
-        designs
-            .iter()
-            .filter(|&it| ways_to_make(it, &patterns) != 0)
-            .count() as u32,
-    )
+    let patterns: HashSet<_> = patterns_str.split(", ").collect();
+    let max_pattern_len = patterns.iter().map(|it| it.len()).max().unwrap();
+
+    let pattern_data = PatternData {
+        patterns,
+        max_pattern_len,
+    };
+    (pattern_data, designs.par_lines())
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (data, designs) = parse(input);
+    let count = designs.filter(|d| data.is_possible(d)).count();
+    Some(count as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let (patterns, designs) = parse(input);
-
-    Some(designs.iter().map(|&it| ways_to_make(it, &patterns)).sum())
+    let (data, designs) = parse(input);
+    Some(designs.map(|d| data.ways_to_make(d)).sum())
 }
 
 #[cfg(test)]
