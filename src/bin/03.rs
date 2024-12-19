f48523f46581e055520367048a5474f690f3d91e
Juha Komulainen <juha.komulainen@evident.fi>
2024-12-19 23:15:06 +0200

    Optimize

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,43 +1,78 @@
-use regex::Regex;
-
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let sum = re
-        .captures_iter(input)
-        .map(|cap| {
-            let x = cap[1].parse::<i32>().unwrap();
-            let y = cap[2].parse::<i32>().unwrap();
-            x * y
-        })
-        .sum::<i32>();
-    Some(sum as u32)
+#[allow(non_camel_case_types)]
+#[allow(clippy::upper_case_acronyms)]
+#[derive(Eq, PartialEq, Copy, Clone)]
+#[rustfmt::skip]
+enum State {
+    E, M, MU, MUL, MULP, NUM1, COMMA, NUM2, D, DO, DOP, DON, DON_, DON_T, DON_TP,
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let re = Regex::new(r"(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))").unwrap();
+fn value(b: u8) -> u32 {
+    (b - b'0') as u32
+}
+
+fn solve(input: &str, part2: bool) -> u32 {
+    use State::*;
+
+    let mut num1: u32 = 0;
+    let mut num2: u32 = 0;
+    let mut sum: u32 = 0;
 
     let mut enabled = true;
-    let mut sum = 0;
+    let mut state = E;
 
-    for cap in re.captures_iter(input) {
-        if let Some(do_match) = cap.get(0) {
-            match do_match.as_str() {
-                "do()" => enabled = true,
-                "don't()" => enabled = false,
-                _ => {
-                    if enabled {
-                        let x = cap[2].parse::<i32>().unwrap();
-                        let y = cap[3].parse::<i32>().unwrap();
-                        sum += x * y;
-                    }
-                }
+    for b in input.bytes() {
+        macro_rules! reset {
+            () => { if enabled && b == b'm' { M } else if part2 && b == b'd' { D } else { E } };
+        }
+
+        macro_rules! expect {
+            ($expected:expr, $next:ident) => {
+                if b == $expected { $next } else { reset!() }
             }
         }
-    }
 
-    Some(sum as u32)
+        macro_rules! expect_first_digit {
+            ($next:ident, $var:ident) => {
+                if b.is_ascii_digit() { $var = value(b); $next } else { reset!() }
+            };
+        }
+
+        macro_rules! expect_next_digit {
+            ($next:ident, $var:ident, $after:expr, $after_state:expr) => {
+                if b.is_ascii_digit() { $var = $var * 10 + value(b); $next } else if b == $after { $after_state } else { reset!() }
+            };
+        }
+
+        state = match state {
+            E => reset!(),
+            M => expect!(b'u', MU),
+            MU => expect!(b'l', MUL),
+            MUL => expect!(b'(', MULP),
+            MULP => expect_first_digit!(NUM1, num1),
+            NUM1 => expect_next_digit!(NUM1, num1, b',', COMMA),
+            COMMA => expect_first_digit!(NUM2, num2),
+            NUM2 => expect_next_digit!(NUM2, num2, b')', { sum += num1 * num2; E} ),
+            D => expect!(b'o', DO),
+            DO => if b == b'n' { DON } else if b == b'(' { DOP } else { reset!() },
+            DOP => if b == b')' { enabled = true; E } else { reset!() },
+            DON => expect!(b'\'', DON_),
+            DON_ => expect!(b't', DON_T),
+            DON_T => expect!(b'(', DON_TP),
+            DON_TP => if b == b')' { enabled = false; E } else { reset!() },
+        };
+    }
+    sum
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(solve(input, false))
+}
+
+#[rustfmt::skip]
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(solve(input, true))
 }
 
 #[cfg(test)]
